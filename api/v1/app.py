f1984acbce79b10c2b8a7228177886eda6e14855
kwhit2 <kfw2@outlook.com>
2021-05-08 00:40:48 +0000

    Task 4 ran in the container

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Setting up API """
+
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+import os
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True  # for printing
+app.register_blueprint(app_views)  # register BP app_views 2 Flask instance app
+HBNB_API_HOST = os.getenv("HBNB_API_HOST")
+HBNB_API_PORT = os.getenv("HBNB_API_PORT")
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ teardown: declare a method to handle @app.teardown_appcontext that
+        calls storage.close()"""
+    storage.close()
+
+if __name__ == '__main__':
+    if not HBNB_API_HOST:
+        HBNB_API_HOST = "0.0.0.0"
+    if not HBNB_API_PORT:
+        HBNB_API_PORT = "5000"
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" index.py """
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status')
+def status():
+    """ returns a JSON: "status": "OK" """
+    return (jsonify({'status': 'OK'}), 200)
